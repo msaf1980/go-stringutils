0fb9558e32d1c776b0399eaaac09e0f834cf28a3
msaf1980 <msaf1980@gmail.com>
2022-09-03 09:50:38 +0500

    deprecate SplitN with Split

--- split_test.go
+++ split_test.go
@@ -39,7 +39,7 @@ func Test_Split2(t *testing.T) {
 }
 
 // for compare with Benchmark_Split2
-func Benchmark_Split(b *testing.B) {
+func Benchmark_Strings_Split2(b *testing.B) {
 	s := "teststring&where"
 
 	b.ReportAllocs()
@@ -61,7 +61,7 @@ func Benchmark_Split2(b *testing.B) {
 	}
 }
 
-func TestSplitN(t *testing.T) {
+func TestSplit(t *testing.T) {
 	buf := make([]string, 4)
 	tests := []struct {
 		s    string
@@ -74,34 +74,110 @@ func TestSplitN(t *testing.T) {
 		{"test1.2.test3.", ".", []string{"test1", "2", "test3", ""}},
 		{"test1=.2.test3.", "=.", []string{"test1", "2.test3."}},
 		{"test1.2.test3.4", ".", []string{"test1", "2", "test3", "4"}},
-		{"test1.2.test3.4.", ".", []string{"test1", "2", "test3", "4."}},
-		{"test1.2.test3.4.5", ".", []string{"test1", "2", "test3", "4.5"}},
-		{"тестПА1ПА2Пк3ПА4ПА5", "ПА", []string{"тест", "1", "2Пк3", "4ПА5"}},
+		{"test1.2.test3.4.", ".", []string{"test1", "2", "test3", "4", ""}},
+		{"test1.2.test3.4.5", ".", []string{"test1", "2", "test3", "4", "5"}},
+		{"тестПА1ПА2Пк3ПА4ПА5", "ПА", []string{"тест", "1", "2Пк3", "4", "5"}},
 	}
 	for _, tt := range tests {
-		t.Run(tt.s, func(t *testing.T) {
-			got := SplitN(tt.s, tt.sep, buf)
+		t.Run(tt.s+" -> "+tt.sep, func(t *testing.T) {
+			got := Split(tt.s, tt.sep, buf)
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("SplitN() = %v, want %v", got, tt.want)
+				t.Errorf("SplitN() = %#v, want %#v", got, tt.want)
 			}
 		})
 	}
 }
 
-func Benchmark_SplitN(b *testing.B) {
+func TestSplitByte(t *testing.T) {
 	buf := make([]string, 4)
+	tests := []struct {
+		s    string
+		sep  byte
+		want []string
+	}{
+		{"", ',', []string{""}},
+		{"test", ',', []string{"test"}},
+		{"test1,2", ',', []string{"test1", "2"}},
+		{"test1.2.test3.", '.', []string{"test1", "2", "test3", ""}},
+		{"test1.2.test3.4", '.', []string{"test1", "2", "test3", "4"}},
+		{"test1.2.test3.4.", '.', []string{"test1", "2", "test3", "4", ""}},
+		{"test1.2.test3.4.5", '.', []string{"test1", "2", "test3", "4", "5"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.s+" -> "+string(tt.sep), func(t *testing.T) {
+			got := SplitByte(tt.s, tt.sep, buf)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SplitN() = %#v, want %#v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSplitRune(t *testing.T) {
+	buf := make([]string, 4)
+	tests := []struct {
+		s    string
+		sep  rune
+		want []string
+	}{
+		{"", ',', []string{""}},
+		{"test", ',', []string{"test"}},
+		{"test1,2", ',', []string{"test1", "2"}},
+		{"test1.2.test3.", '.', []string{"test1", "2", "test3", ""}},
+		{"test1.2.test3.4", '.', []string{"test1", "2", "test3", "4"}},
+		{"test1.2.test3.4.", '.', []string{"test1", "2", "test3", "4", ""}},
+		{"test1.2.test3.4.5", '.', []string{"test1", "2", "test3", "4", "5"}},
+		{"тестПА1ПА2Пк3ПА4ПА5", 'П', []string{"тест", "А1", "А2", "к3", "А4", "А5"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.s+" -> "+string(tt.sep), func(t *testing.T) {
+			got := SplitRune(tt.s, tt.sep, buf)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SplitN() = %#v, want %#v", got, tt.want)
+			}
+		})
+	}
+}
+
+// degraded, reallocated buffer not reused, use strings.Split for this case
+func Benchmark_SplitRealloc(b *testing.B) {
+	buf := make([]string, 1)
 	s := "test1.2.test3.4.5"
 
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		ss := SplitN(s, ".", buf)
-		_ = ss
+		_ = Split(s, ".", buf)
 	}
 }
 
-func Benchmark_SplitN_Check(b *testing.B) {
+// reuse reallocated buffer
+func Benchmark_SplitReuse(b *testing.B) {
+	buf := make([]string, 1)
+	s := "test1.2.test3.4.5"
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf = Split(s, ".", buf)
+	}
+}
+
+func Benchmark_SplitWReuse(b *testing.B) {
+	buf := make([]string, 1)
+	s := "тестПА1ПА2Пк3ПА4ПА5"
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf = Split(s, "ПА", buf)
+	}
+}
+
+func Benchmark_Split_Check(b *testing.B) {
 	buf := make([]string, 1)
 	s := "test1.2.test3.4.5"
 
@@ -113,8 +189,7 @@ func Benchmark_SplitN_Check(b *testing.B) {
 		if len(buf) < count {
 			buf = make([]string, count+1)
 		}
-		ss := SplitN(s, ".", buf)
-		_ = ss
+		_ = Split(s, ".", buf)
 	}
 }
 
@@ -127,5 +202,44 @@ func Benchmark_Strings_Split(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ss := strings.Split(s, ".")
 		_ = ss
+	}
+}
+
+// reuse reallocated buffer
+func Benchmark_SplitByteReuse(b *testing.B) {
+	buf := make([]string, 1)
+	s := "test1.2.test3.4.5"
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf = SplitByte(s, '.', buf)
+	}
+}
+
+// reuse reallocated buffer
+func Benchmark_SplitRuneReuse(b *testing.B) {
+	buf := make([]string, 1)
+	s := "test1.2.test3.4.5"
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf = SplitRune(s, '.', buf)
+	}
+}
+
+// reuse reallocated buffer, unicode rune
+func Benchmark_SplitRuneWReuse(b *testing.B) {
+	buf := make([]string, 1)
+	s := "тестПА1ПА2Пк3ПА4ПА5"
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		buf = SplitRune(s, 'П', buf)
 	}
 }

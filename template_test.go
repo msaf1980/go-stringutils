de5fc556def5ba7944bf8840d3aa0be1a7942a61
msaf1980 <msaf1980@gmail.com>
2022-09-19 12:49:44 +0500

    template: partial execute (with incomplete parameters)

--- template_test.go
+++ template_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTemplate_InitTemplate(t *testing.T) {
+func TestTemplate_NewTemplate(t *testing.T) {
 	tests := []struct {
 		format   string
 		template Template
@@ -35,11 +35,11 @@ func TestTemplate_InitTemplate(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.format, func(t *testing.T) {
-			tpl, err := InitTemplate(tt.format)
+			tpl, err := NewTemplate(tt.format)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("InitTemplate() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("NewTemplate() error = %v, wantErr %v", err, tt.wantErr)
 			}
-			assert.Equalf(t, tt.template, tpl, "InitTemplate() results mismatch")
+			assert.Equalf(t, tt.template, tpl, "NewTemplate() results mismatch")
 		})
 	}
 }
@@ -91,13 +91,76 @@ func TestTemplate_Execute(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.format, func(t *testing.T) {
-			tpl, err := InitTemplate(tt.format)
+			tpl, err := NewTemplate(tt.format)
 			if err != nil {
-				t.Fatalf("InitTemplate() error = %v", err)
+				t.Fatalf("NewTemplate() error = %v", err)
 			}
 			got, err := tpl.Execute(params)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Template.Execute() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Template.Execute() = '%v', want '%v'", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTemplate_ExecutePartial(t *testing.T) {
+	params := map[string]interface{}{
+		"query":  "URL",
+		"status": "success",
+		"time":   "now",
+		"one": map[string]interface{}{
+			"two_one": "2_1", "two_two": "2_2",
+			"three": map[string]interface{}{"three_four": "3_4"},
+		},
+		"int32":   int32(-1),
+		"uint32":  uint32(1),
+		"int64":   int64(-2),
+		"uint64":  uint64(2),
+		"int16":   int16(-1),
+		"uint16":  uint16(1),
+		"int8":    int8(-1),
+		"uint8":   uint8(1),
+		"int":     -1,
+		"uint":    1,
+		"float32": 1.3,
+		"float64": 2.4,
+	}
+	tests := []struct {
+		format      string
+		want        string
+		wantPartial bool
+	}{
+		// one element
+		{format: "", want: "", wantPartial: false},
+		{format: "test", want: "test", wantPartial: false},
+		{format: "%{query}", want: "URL", wantPartial: false},
+		{format: "%{not_exist}", want: "%{not_exist}", wantPartial: true},
+		//several elements
+		{format: "%{query} %{status}", want: "URL success", wantPartial: false},
+		{format: "%{query} %{status} at %{time}", want: "URL success at now", wantPartial: false},
+		// deep params
+		{format: "%{query} %{one.two_one} %{one.three.three_four}", want: "URL 2_1 3_4", wantPartial: false},
+		{format: "%{query} %{one.two_one} %{one.three.three_four.2}", want: "URL 2_1 %{one.three.three_four.2}", wantPartial: true},
+		{format: "%{query} %{one.two_one} %{one.three}", want: "URL 2_1 %{one.three}", wantPartial: true},
+		// non-string data types
+		{format: "%{int32} %{uint32} %{int64} %{uint64} %{float32} %{float64}", want: "-1 1 -2 2 1.3 2.4", wantPartial: false},
+		{format: "%{int16} %{uint16}", want: "-1 1", wantPartial: false},
+		{format: "%{int8} %{uint8}", want: "-1 1", wantPartial: false},
+		{format: "%{int} %{uint}", want: "-1 1", wantPartial: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.format, func(t *testing.T) {
+			tpl, err := NewTemplate(tt.format)
+			if err != nil {
+				t.Fatalf("NewTemplate() error = %v", err)
+			}
+			got, part := tpl.ExecutePartial(params)
+			if part != tt.wantPartial {
+				t.Errorf("Template.ExecutePartial() partial = %v, want %v", err, tt.wantPartial)
 				return
 			}
 			if got != tt.want {
@@ -118,9 +181,9 @@ func Benchmark_TemplateExecute(b *testing.B) {
 		},
 	}
 	format := "%{query} %{one.two_one} %{one.three.three_four}"
-	tpl, err := InitTemplate(format)
+	tpl, err := NewTemplate(format)
 	if err != nil {
-		b.Fatalf("InitTemplate() error = %v", err)
+		b.Fatalf("NewTemplate() error = %v", err)
 	}
 	for i := 0; i < b.N; i++ {
 		got, err := tpl.Execute(params)
@@ -144,9 +207,9 @@ func Benchmark_TemplateInitAndExecute(b *testing.B) {
 	}
 	format := "%{query} %{one.two_one} %{one.three.three_four}"
 	for i := 0; i < b.N; i++ {
-		tpl, err := InitTemplate(format)
+		tpl, err := NewTemplate(format)
 		if err != nil {
-			b.Fatalf("InitTemplate() error = %v", err)
+			b.Fatalf("NewTemplate() error = %v", err)
 		}
 		got, err := tpl.Execute(params)
 		_ = got

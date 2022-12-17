25881aff012ad761a2a795908ad5ee744aba87c0
msaf1980 <msaf1980@gmail.com>
2022-12-17 13:15:25 +0500

    compability with golang 1.20 unsafe.String proposal

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/msaf1980/go-stringutils
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0 // indirect
+require github.com/stretchr/testify v1.7.0
--- unsafe_test.go
+++ unsafe_test.go
@@ -13,11 +13,11 @@ func TestUnsafeString(t *testing.T) {
 	}
 }
 
-func TestUnsafeStringFromPtr(t *testing.T) {
+func TestString(t *testing.T) {
 	s := "test"
 	bytes := []byte(s)
-	if got := UnsafeStringFromPtr(&bytes[0], len(bytes)); got != s {
-		t.Errorf("UnsafeStringFromPtr() = '%s', want '%s'", got, s)
+	if got := String(&bytes[0], len(bytes)); got != s {
+		t.Errorf("String()) = '%s', want '%s'", got, s)
 	}
 }
 
@@ -26,5 +26,61 @@ func TestUnsafeStringBytes(t *testing.T) {
 	s := string(bytes)
 	if got := UnsafeStringBytes(&s); !reflect.DeepEqual(got, bytes) {
 		t.Errorf("UnsafeStringBytes() = '%v', want '%v'", got, bytes)
+	}
+}
+
+func TestStringData(t *testing.T) {
+	bytes := []byte("test")
+	s := string(bytes)
+	length := len(s)
+	got := StringData(s)
+	gotBytes := UnsafeBytes(got, length, length)
+	if !reflect.DeepEqual(gotBytes, bytes) {
+		t.Errorf("UnsafeStringBytes() = '%v' (%q), want '%v' (%q)", gotBytes, string(gotBytes), bytes, string(bytes))
+	}
+}
+
+func Benchmark_UnsafeString(b *testing.B) {
+	bytes := []byte("test1.2.test3.4.5")
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = UnsafeString(bytes)
+	}
+}
+
+func Benchmark_String(b *testing.B) {
+	s := "test1.2.test3.4.5"
+	bytes := []byte(s)
+	ptr := &bytes[0]
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = String(ptr, len(bytes))
+	}
+}
+
+func Benchmark_UnsafeStringBytes(b *testing.B) {
+	s := "test1.2.test3.4.5"
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = UnsafeStringBytes(&s)
+	}
+}
+
+func Benchmark_UnsafeStringBytePtr(b *testing.B) {
+	s := "test1.2.test3.4.5"
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = UnsafeStringBytePtr(s)
 	}
 }
